2760b6cf760377906258b34e8c324cd04298e4b0
kellykamunya <132022927+kellykamunya@users.noreply.github.com>
2024-02-26 10:06:39 -0800

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,5 @@
+#first Lms assignment 1
+name=input("enter your names:")
+age=int(input("what is your age:"))
+location=input("state your location please:")
+print(f"my name is {name} {age} years of age and I live am in {location}")
