29315dbc7421c7edd5affe06d35ec8da81729a34
kellykamunya <132022927+kellykamunya@users.noreply.github.com>
2024-03-13 09:05:22 -0800

    Add files via upload

---
+++ discountcalc.py
@@ -0,0 +1,20 @@
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        discounted_price = price - (price * (discount_percent / 100))
+        return discounted_price
+    else:
+        return price
+
+def main():
+    original_price = float(input("Enter the original price of the item: Kes "))
+    discount_percent = float(input("Enter the discount percentage: "))
+    
+    final_price = calculate_discount(original_price, discount_percent)
+    
+    if final_price == original_price:
+        print(f"No discount applied. Final price: Kes{final_price:.2f}")
+    else:
+        print(f"Discount applied. Final price after discount: kes{final_price:.2f}")
+
+if __name__ == "__main__":
+    main()
